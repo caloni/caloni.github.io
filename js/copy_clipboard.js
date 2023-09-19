4802a4132a0b5737eaabdeab1b714d12e24d2b49
Wanderley Caloni <wcaloni@venn.com>
2023-09-19 08:44:39 -0300

    Publish new version.

--- js/copy_clipboard.js
+++ js/copy_clipboard.js
@@ -1,6 +1,9 @@
 function copy_clipboard(id) {
   var copyText = document.querySelector(id).textContent;
-  copyText = copyText.replace(" ^ ctrl_c","")
+  if( copyText[0] == "\n" ) {
+    copyText = copyText.substring(1);
+  }
+  copyText = copyText.replace(" ^ ctrl_c"," [by caloni]")
   copyText = copyText.replace(" [link]","")
   navigator.clipboard.writeText(copyText);
 }
