505bdc1b3909e05ba411634ea0c55ab17df2792c
Wanderley Caloni <wcaloni@venn.com>
2023-09-25 00:02:58 -0300

    Publish new version.

--- js/copy_clipboard.js
+++ js/copy_clipboard.js
@@ -3,8 +3,15 @@ function copy_clipboard(id) {
   if( copyText[0] == "\n" ) {
     copyText = copyText.substring(1);
   }
-  copyText = copyText.replace(" ^ ctrl_c"," [by caloni]")
+  if( copyText[0] == "#" ) {
+    copyText = copyText.substring(2);
+  }
+  copyText = copyText.replace(" ^ ctrl_c","")
   copyText = copyText.replace(" [link]","")
+  afterTitle = copyText.search("\n")
+  if( afterTitle != -1 ) {
+    copyText = copyText.slice(0, afterTitle) + "\nWanderley Caloni" + copyText.slice(afterTitle);
+  }
   navigator.clipboard.writeText(copyText);
 }
 
