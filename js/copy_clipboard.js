1b26b686867de4d5b798be7e6cc8bd227ab63a3b
Wanderley Caloni <wanderley.caloni@gmail.com>
2024-08-06 20:22:41 -0300

    Here comes great news.

--- js/copy_clipboard.js
+++ js/copy_clipboard.js
@@ -1,14 +1,4 @@
 function copy_clipboard(id) {
-  var buttonCopy = " \\^ ctrl_c";
-  var copyText = document.querySelector(id).textContent.substring(3);
-  var afterTitle = copyText.search("\n");
-  var afterHeader = copyText.search(buttonCopy);
-  var title = copyText.substring(0, afterTitle);
-  var header = copyText.substring(afterTitle + 1, afterHeader);
-  var content = copyText.substring(afterHeader + buttonCopy.length + 1);
-
-  content = content.replace(/(?:\r\n|\r|\n)/g, "\n\n");
-  copyText = title + "\nWanderley Caloni, " + header + "\n\n" + content;
+  var copyText = document.querySelector(id).textContent.replace(/^\s+|\s+$/g, '');
   navigator.clipboard.writeText(copyText);
 }
-
